f9a8fb7adb99f472a2439fa7946f942e1b1ba2e0
Javier Chatruc <jrchatruc@gmail.com>
2023-06-26 12:46:58 -0300

    printf stuff

--- c_code/src/test_sve.c
+++ c_code/src/test_sve.c
@@ -326,7 +326,7 @@ void print_uint_array(size_t len, uint64_t arr[len])
 	printf("[");
 	for (size_t i = 0; i < len; i++)
 	{
-		printf("%llu ", arr[i]);
+		printf("%lu ", arr[i]);
 	}
 
 	printf("]\n");
