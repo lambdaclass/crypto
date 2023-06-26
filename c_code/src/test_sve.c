16857b238ff40a7564e63521156879921f6be3f0
Javier Chatruc <jrchatruc@gmail.com>
2023-06-26 12:47:49 -0300

    uncoment stuff

--- c_code/src/test_sve.c
+++ c_code/src/test_sve.c
@@ -332,16 +332,16 @@ void print_uint_array(size_t len, uint64_t arr[len])
 	printf("]\n");
 }
 
-// void daxpy_1_1_sve(int64_t n, double da, double *dx, double *dy)
-// {
-//     int64_t i = 0;
-//     svbool_t pg = svwhilelt_b64(i, n); // [1]
-//     do
-//     {
-//         svfloat64_t dx_vec = svld1(pg, &dx[i]);             // [2]
-//         svfloat64_t dy_vec = svld1(pg, &dy[i]);             // [2]
-//         svst1(pg, &dy[i], svmla_x(pg, dy_vec, dx_vec, da)); // [3]
-//         i += svcntd();                                      // [4]
-//         pg = svwhilelt_b64(i, n);                           // [1]
-//     } while (svptest_any(svptrue_b64(), pg));               // [5]
-// }
+void daxpy_1_1_sve(int64_t n, double da, double *dx, double *dy)
+{
+	int64_t i = 0;
+	svbool_t pg = svwhilelt_b64(i, n); // [1]
+	do
+	{
+		svfloat64_t dx_vec = svld1(pg, &dx[i]);             // [2]
+		svfloat64_t dy_vec = svld1(pg, &dy[i]);             // [2]
+		svst1(pg, &dy[i], svmla_x(pg, dy_vec, dx_vec, da)); // [3]
+		i += svcntd();                                      // [4]
+		pg = svwhilelt_b64(i, n);                           // [1]
+	} while (svptest_any(svptrue_b64(), pg));                   // [5]
+}
